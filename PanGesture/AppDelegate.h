6df62039c0467b058dd3154733faffa720a9eec6
zhangzhaopds <zhangzhaopds@foxmail.com>
2016-03-03 13:41:00 +0800

    Initial Commit

---
+++ PanGesture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PanGesture
+//
+//  Created by 张昭 on 16/3/3.
+//  Copyright © 2016年 张昭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PanGesture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PanGesture
+//
+//  Created by 张昭 on 16/3/3.
+//  Copyright © 2016年 张昭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
